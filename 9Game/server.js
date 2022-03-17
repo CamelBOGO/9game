24c17d4a957911d1a1815952f8cd229480d3ac4d
WaYiu <1155129302@link.cuhk.edu.hk>
2022-03-17 11:31:12 +0800

    Clean style, bug fix
    login/register ok
    database collect data ok
    problem: redirect (TO-FIX)

--- 9Game/server.js
+++ 9Game/server.js
@@ -1,52 +1,56 @@
-const express = require("express");
-const app = express();
+const express = require("express")
+const app = express()
 
-const formidable = require("express-formidable");
-app.use(formidable());
+const formidable = require("express-formidable")
+app.use(formidable())
 
-const mongodb = require("mongodb");
-const mongoClient = mongodb.MongoClient;
+const mongodb = require("mongodb")
+const mongoClient = mongodb.MongoClient
 const dotenv = require("dotenv")
-const ObjectId = mongodb.ObjectId;
+const ObjectId = mongodb.ObjectId
 
-const http = require("http").createServer(app);
-const bcrypt = require("bcrypt");
-const fileSystem = require("fs");
+const http = require("http").createServer(app)
+const bcrypt = require("bcrypt")
+const fileSystem = require("fs")
 
-const jwt = require("jsonwebtoken");
-const accessTokenSecret = "accessTokenSecret"; 
+const jwt = require("jsonwebtoken")
+const accessTokenSecret = "accessTokenSecret" 
 
-app.use("/public", express.static(__dirname + "/public"));
-app.set("view engine", "ejs");
+app.use("/public", express.static(__dirname + "/public"))
+app.set("view engine", "ejs")
 
-const socketIO = require("socket.io")(http);
-const socketID = "";
-const users = [];
+const socketIO = require("socket.io")(http)
+const socketID = ""
+const users = []
 
-const mainURL = "http://localhost:3000";
+const mainURL = "http://localhost:3000"
 
 socketIO.on("connection", function (socket) {
-	console.log("User connected", socket.id);
-	socketID = socket.id;
-});
+	console.log("User connected", socket.id)
+	socketID = socket.id
+})
 
 dotenv.config()
 http.listen(3000, function(){
-    console.log("Server Started");   
+    console.log("Server Started")   
 
 	mongoClient.connect(process.env.MONGO_URL, { useNewUrlParser: true}, function (error, client) {
-		const database = client.db("9Game");
-		console.log("Database connected.");
+		const database = client.db("9Game")
+		console.log("Database connected.")
 
-		app.get("/register", function(request, result){
-			result.render("register");
-		});
 
-		app.post("/register", function(request,result){
-			const name = request.fields.name;
-			const username = request.fields.username;
-			const email = request.fields.email;
-			const password = request.fields.password;
+		//****************************************************************************************/
+		app.get("/register", function(req, res){
+			res.render("register")
+		})
+
+
+		app.post("/register", function (req, res) {
+			const name = req.fields.name
+			const username = req.fields.username
+			const email = req.fields.email
+			const password = req.fields.password
+			const reset_token = ""
 
 			database.collection("users").findOne({
 				$or: [{
@@ -54,7 +58,7 @@ http.listen(3000, function(){
 				}, {
 					"username": username
 				}]
-			}, function (error, user) {
+			}, (error, user) => {
 				if (user == null) {
 					bcrypt.hash(password, 10, function (error, hash) {
 						database.collection("users").insertOne({
@@ -62,20 +66,69 @@ http.listen(3000, function(){
 							"username": username,
 							"email": email,
 							"password": hash,
+							"reset_token": reset_token,
 						}, function (error, data) {
-							result.json({
+							res.json({
 								"status": "success",
-								"message": "Register successfully."
-							});
-						});
-					});
+								"message": "Signed up successfully."
+							})
+						})
+					})
 				} else {
-					result.json({
+					res.json({
 						"status": "error",
 						"message": "Email or username already exist."
-					});
+					})
 				}
-			});	
+			})
 		})
+		//****************************************************************************************/
+
+		//****************************************************************************************/
+		app.get("/login", (req, res) => {
+			res.render("login.ejs")
+		})
+
+		app.post("/login", (req, res) => {
+			 email = req.fields.email
+			const password = req.fields.password
+			database.collection("users").findOne({
+				"email": email
+			}, (error, user) => {
+				if (user == null) {
+					res.json({
+						"status": "error",
+						"message": "Email does not exist"
+					})
+				} else {
+					bcrypt.compare(password, user.password, (error, isVerify) => {
+						if (isVerify) {
+							const accessToken = jwt.sign({ email: email }, accessTokenSecret)
+							database.collection("users").findOneAndUpdate({
+								"email": email
+							}, {
+								$set: {
+									"accessToken": accessToken
+								}
+							}, (error, data) => {
+								res.json({
+									"status": "success",
+									"message": "Login successfully",
+									"accessToken": accessToken,
+									"profileImage": user.profileImage
+								})
+							})
+						} else {
+							res.json({
+								"status": "error",
+								"message": "Password is not correct"
+							})
+						}
+					})
+				}
+			})
+		})
+		//****************************************************************************************/
+
     })
 })
