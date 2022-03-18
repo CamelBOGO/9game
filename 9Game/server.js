2f750c29b7b3c3f78c868db990f45b00bc7d6478
WaYiu <1155129302@link.cuhk.edu.hk>
2022-03-19 01:08:48 +0800

    Email Verification
    Email Verification Done
    TO-DO: Forget Password

--- 9Game/server.js
+++ 9Game/server.js
@@ -49,6 +49,21 @@ app.get("/index", (req, res) => {
 })
 
 
+//verifying email
+const nodemailer = require("nodemailer")
+const nodemailerFrom = "CSCI3100GpE2@gmail.com"
+const nodemailerObject = {	
+	service: "gmail",
+	host: "smtp.gmail.com",
+	port: 465,
+	secure: true,
+	auth: {
+		user: "CSCI3100GpE2@gmail.com",
+		pass: "Csci3100E2"
+	}
+}
+
+
 //To server//
 mongoClient.connect(process.env.MONGO_URL, { useNewUrlParser: true}, (error, client) => {
 	const database = client.db("9Game")
@@ -60,6 +75,8 @@ mongoClient.connect(process.env.MONGO_URL, { useNewUrlParser: true}, (error, cli
 		const email = req.fields.email
 		const password = req.fields.password
 		const reset_token = ""
+		const isVerified = false;
+		const verification_token = new Date().getTime()
 
 		database.collection("users").findOne({
 			$or: [{
@@ -76,7 +93,28 @@ mongoClient.connect(process.env.MONGO_URL, { useNewUrlParser: true}, (error, cli
 						"email": email,
 						"password": hash,
 						"reset_token": reset_token,
+						"isVerified": isVerified,
+						"verification_token": verification_token
 					}, (error, data) => {
+
+						const transporter = nodemailer.createTransport(nodemailerObject)
+						const text = "Please verify your account by clicking the following link: " + mainURL + "/verifyingEmail/" + email + "/" + verification_token
+						const html = "Please verify your account by clicking the following link: <br><br> <a href='" + mainURL + "/verifyingEmail/" + email + "/" + verification_token + "'>Confirm Email</a> <br><br> Thank You."
+
+						transporter.sendMail({
+							from: nodemailerFrom,
+							to: email,
+							subject: "Email Verification",
+							text: text,
+							html: html
+						}, function (error, info) {
+							if (error){
+								console.error(error)
+							} else {
+								console.log("Email sent: " + info.response)
+							}
+						})
+
 						res.json({
 							"status": "success",
 							"message": "Signed up successfully."
@@ -95,6 +133,7 @@ mongoClient.connect(process.env.MONGO_URL, { useNewUrlParser: true}, (error, cli
 	app.post("/login", (req, res) => {
 		const username = req.fields.username
 		const password = req.fields.password
+
 		database.collection("users").findOne({
 			"username": username
 		}, (error, user) => {
@@ -102,25 +141,31 @@ mongoClient.connect(process.env.MONGO_URL, { useNewUrlParser: true}, (error, cli
 				res.json({
 					"status": "error",
 					"message": "User does not exist"
-				})
-			} else {
+				})} else {
 				bcrypt.compare(password, user.password, (error, isVerify) => {
 					if (isVerify) {
-						const accessToken = jwt.sign({ username: username }, accessTokenSecret)
-						database.collection("users").findOneAndUpdate({
-							"username": username
-						}, {
-							$set: {
-								"accessToken": accessToken
-							}
-						}, (error, data) => {
-							res.json({
-								"status": "success",
-								"message": "Login successfully",
-								"accessToken": accessToken,
-								"profileImage": user.profileImage
+						if (user.isVerified){
+							const accessToken = jwt.sign({ username: username }, accessTokenSecret)
+							database.collection("users").findOneAndUpdate({
+								"username": username
+							}, {
+								$set: {
+									"accessToken": accessToken
+								}
+							}, (error, data) => {
+								res.json({
+									"status": "success",
+									"message": "Login successfully",
+									"accessToken": accessToken,
+									"profileImage": user.profileImage
+								})
 							})
-						})
+						} else {
+							res.json({
+								"status": "error",
+								"message": "Email Not Verified",
+							})
+					}
 					} else {
 						res.json({
 							"status": "error",
@@ -131,6 +176,44 @@ mongoClient.connect(process.env.MONGO_URL, { useNewUrlParser: true}, (error, cli
 			}
 		})
 	})
+
+	app.get("/verifyingEmail/:email/:verification_token", async(req, res) => {
+		const email = req.params.email
+		const verification_token = req.params.verification_token
+		const user = await database.collection("users").findOne({
+			$and: [{
+				"email": email,
+			}, {
+				"verification_token": parseInt(verification_token)
+			}]
+		})
+
+		if (user == null && false){
+			req.status = "error"
+			req.message = "Email does not exists or link is expired."
+			res.render("login.ejs", {
+				"request": req
+			})
+		} else {
+			await database.collection("users").findOneAndUpdate({
+				$and: [{
+					"email": email,
+				}, {
+					"verification_token": parseInt(verification_token)
+				}]
+			}, {
+				$set: {
+					"verification_token": "",
+					"isVerified": true
+				}
+			})
+
+			req.status = "success"
+			req.message = "Account has been activitated"
+			res.render("login.ejs", {"request": req})
+		}
+	})
+
 })
 
 
