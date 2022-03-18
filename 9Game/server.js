566629f89d09a1b9d7e7f852bed1094ee3bc2838
WaYiu <1155129302@link.cuhk.edu.hk>
2022-03-18 13:15:07 +0800

    Login/Register Finish
    Finish main part of Login/Register
    Finish Design
    TO-DO: Verification Email, Forget Email, Forget Page

--- 9Game/server.js
+++ 9Game/server.js
@@ -25,110 +25,117 @@ const users = []
 
 const mainURL = "http://localhost:3000"
 
-socketIO.on("connection", function (socket) {
+dotenv.config()
+
+socketIO.on("connection", (socket) => {
 	console.log("User connected", socket.id)
 	socketID = socket.id
 })
 
-dotenv.config()
-http.listen(3000, function(){
-    console.log("Server Started")   
+app.get("/", (req, res) => {
+	res.redirect("/login")
+})
 
-	mongoClient.connect(process.env.MONGO_URL, { useNewUrlParser: true}, function (error, client) {
-		const database = client.db("9Game")
-		console.log("Database connected.")
+app.get("/register", (req, res) => {
+	res.render("register.ejs")
+})
+
+app.get("/login", (req, res) => {
+	res.render("login.ejs")
+})
+
+app.get("/index", (req, res) => {
+	res.render("index.ejs")
+})
 
 
-		//****************************************************************************************/
-		app.get("/register", function(req, res){
-			res.render("register")
-		})
+//To server//
+mongoClient.connect(process.env.MONGO_URL, { useNewUrlParser: true}, (error, client) => {
+	const database = client.db("9Game")
+	console.log("Database connected.")
 
+	app.post("/register", (req, res) => {
+		const name = req.fields.name
+		const username = req.fields.username
+		const email = req.fields.email
+		const password = req.fields.password
+		const reset_token = ""
 
-		app.post("/register", function (req, res) {
-			const name = req.fields.name
-			const username = req.fields.username
-			const email = req.fields.email
-			const password = req.fields.password
-			const reset_token = ""
-
-			database.collection("users").findOne({
-				$or: [{
-					"email": email
-				}, {
-					"username": username
-				}]
-			}, (error, user) => {
-				if (user == null) {
-					bcrypt.hash(password, 10, function (error, hash) {
-						database.collection("users").insertOne({
-							"name": name,
-							"username": username,
-							"email": email,
-							"password": hash,
-							"reset_token": reset_token,
-						}, function (error, data) {
-							res.json({
-								"status": "success",
-								"message": "Signed up successfully."
-							})
+		database.collection("users").findOne({
+			$or: [{
+				"email": email
+			}, {
+				"username": username
+			}]
+		}, (error, user) => {
+			if (user == null) {
+				bcrypt.hash(password, 10, (error, hash) => {
+					database.collection("users").insertOne({
+						"name": name,
+						"username": username,
+						"email": email,
+						"password": hash,
+						"reset_token": reset_token,
+					}, (error, data) => {
+						res.json({
+							"status": "success",
+							"message": "Signed up successfully."
 						})
 					})
-				} else {
-					res.json({
-						"status": "error",
-						"message": "Email or username already exist."
-					})
-				}
-			})
+				})
+			} else {
+				res.json({
+					"status": "error",
+					"message": "Email or username already exist."
+				})
+			}
 		})
-		//****************************************************************************************/
+	})
 
-		//****************************************************************************************/
-		app.get("/login", (req, res) => {
-			res.render("login.ejs")
-		})
-
-		app.post("/login", (req, res) => {
-			 email = req.fields.email
-			const password = req.fields.password
-			database.collection("users").findOne({
-				"email": email
-			}, (error, user) => {
-				if (user == null) {
-					res.json({
-						"status": "error",
-						"message": "Email does not exist"
-					})
-				} else {
-					bcrypt.compare(password, user.password, (error, isVerify) => {
-						if (isVerify) {
-							const accessToken = jwt.sign({ email: email }, accessTokenSecret)
-							database.collection("users").findOneAndUpdate({
-								"email": email
-							}, {
-								$set: {
-									"accessToken": accessToken
-								}
-							}, (error, data) => {
-								res.json({
-									"status": "success",
-									"message": "Login successfully",
-									"accessToken": accessToken,
-									"profileImage": user.profileImage
-								})
-							})
-						} else {
+	app.post("/login", (req, res) => {
+		const username = req.fields.username
+		const password = req.fields.password
+		database.collection("users").findOne({
+			"username": username
+		}, (error, user) => {
+			if (user == null) {
+				res.json({
+					"status": "error",
+					"message": "User does not exist"
+				})
+			} else {
+				bcrypt.compare(password, user.password, (error, isVerify) => {
+					if (isVerify) {
+						const accessToken = jwt.sign({ username: username }, accessTokenSecret)
+						database.collection("users").findOneAndUpdate({
+							"username": username
+						}, {
+							$set: {
+								"accessToken": accessToken
+							}
+						}, (error, data) => {
 							res.json({
-								"status": "error",
-								"message": "Password is not correct"
+								"status": "success",
+								"message": "Login successfully",
+								"accessToken": accessToken,
+								"profileImage": user.profileImage
 							})
-						}
-					})
-				}
-			})
+						})
+					} else {
+						res.json({
+							"status": "error",
+							"message": "Password is not correct"
+						})
+					}
+				})
+			}
 		})
-		//****************************************************************************************/
+	})
+})
 
-    })
+
+
+
+http.listen(3000, () => {
+    console.log("9Game Started")   
 })
