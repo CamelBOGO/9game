de5de36340c8b43b3de9fdea3099ce105803ed5b
pka336 <42817357+CamelBOGO@users.noreply.github.com>
2022-04-09 06:39:57 +0800

    Fixing

--- _9game_index/components/card.jsx
+++ _9game_index/components/card.jsx
@@ -1,11 +1,24 @@
 import {Card, CardActionArea, CardContent, CardMedia} from "@mui/material"
 import {Box, Typography} from "@mui/material"
+import {makeStyles} from "@material-ui/styles"
 
-export default function IndexCard({title, content}) {
+
+export default function IndexCard({id, title, content}) {
+    const cardTextStyle = makeStyles({
+        textEllipsis: {
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+            display: "-webkit-box",
+            "-webkit-line-clamp": 12,
+            "-webkit-box-orient": "vertical"
+        }
+    });
+    const classes = cardTextStyle()
+
     return (
-        <Card sx={{width: 300, boxShadow: 5}}>
-            <CardActionArea>
-                <Box sx={{minHeight: 300, m: 2}}>
+        <Card sx={{width: 300, height: 400, boxShadow: 5}}>
+            <CardActionArea href={`/post/${id}/comnt`}>
+                <Box component="div" sx={{height: 300, p: 2}}>
                     {/*<CardMedia
                         component="img"
                         height="140"
@@ -13,16 +26,25 @@ export default function IndexCard({title, content}) {
                         alt={content}
                         sx={{m: 2}}
                     />*/}
-                    <Typography gutterBottom variant="body1" component="div">
+                    <Typography gutterBottom variant="body1"
+                                component="div"
+                                sx={{
+                                    overflow: 'hidden',
+                                    textOverflow: 'ellipsis',
+                                    display: '-webkit-box',
+                                    WebkitLineClamp: '12',
+                                    WebkitBoxOrient: 'vertical',
+                                }}>
                         {content}
                     </Typography>
                 </Box>
                 <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
+                    <Typography noWrap gutterBottom variant="h5" component="div">
                         {title}
                     </Typography>
                 </CardContent>
             </CardActionArea>
         </Card>
-    );
+    )
+        ;
 }
--- _9game_index/db_models/user_model.js
+++ _9game_index/db_models/user_model.js
@@ -2,33 +2,34 @@ import mongoose from "mongoose"
 import validator from "validator"
 
 const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    validate: [validator.isEmail, "Please Enter Valid Email Address"],
-  },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: [validator.isEmail, "Please Enter Valid Email Address"],
+    },
 
-  password: {
-    type: String,
-    required: true
-  },
+    password: {
+        type: String,
+        required: true
+    },
 
-  reset_token: {
-    type: String,
-  },
+    reset_token: {
+        type: String,
+    },
 
-  isVerified: {
-    type: Boolean,
-  },
+    isVerified: {
+        type: Boolean,
+    },
 
-  verification_token: {
-    type: String,
-  },
-  
-  accessToken:{
-    type: String,
-  }
+    verification_token: {
+        type: String,
+    },
+
+    accessToken: {
+        type: String,
+    }
+
 })
 
 
