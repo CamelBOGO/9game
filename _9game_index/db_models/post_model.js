770afb874eb152314a3f25e94757d99e77066f55
Brian Fong <100516213+BriaaaaanFong@users.noreply.github.com>
2022-04-09 07:50:25 +0800

    Update post_model.js

--- _9game_index/db_models/post_model.js
+++ _9game_index/db_models/post_model.js
@@ -3,6 +3,9 @@ import mongoose from "mongoose"
 const PostSchema = new mongoose.Schema({
     title: String,
     content: String,
+    username: String,
+    timestamp: String,
+    postdate: String
 })
 
 export default mongoose.models.Post || mongoose.model('Post', PostSchema, "Posts")
