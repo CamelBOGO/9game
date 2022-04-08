617535b1d9e671b134634f03ad174f72dabfc2e2
Brian Fong <100516213+BriaaaaanFong@users.noreply.github.com>
2022-04-09 04:21:48 +0800

    new_post_popup module

---
+++ _9game_index/components/newPost/new_post_popup.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState} from "react";
+import NewPost from "../../pages/new_post";
+import popupStyles from "./custom-popup.module.css";
+
+const NewPostPopUp = (props) => {
+    return (
+        <div className={popupStyles.overlay}
+        style={{
+            visibility: props.show? "visible" : "hidden",
+            opacity: props.show? "1" : "0"
+        }}>   
+            <div className={popupStyles.modal}>
+                <span className={popupStyles.close} onClick={() => props.onClose(false)}>
+                &times;
+                </span>
+                <div className={popupStyles.content}>
+                    {props.children}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default NewPostPopUp;
