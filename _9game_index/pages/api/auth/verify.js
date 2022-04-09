b95b4533c5e3335c50656cfd6916530c20c799d4
WaYiu <1155129302@link.cuhk.edu.hk>
2022-04-10 03:37:21 +0800

    Verify Done

--- _9game_index/pages/api/auth/verify.js
+++ _9game_index/pages/api/auth/verify.js
@@ -1,25 +1,33 @@
 import dbConnect from "../../../lib/dbConnect";
 import User from "../../../db_models/user_model"
 
+
 export default async (req, res) => {
     await dbConnect()
 
     try {
-        if (req.method == "GET") {
-            const { email, verification_token } = req.body
-            const user = User.findOne({
+        if (req.method == "PUT") {
+            const { essential } = req.body
+            const email = essential.email
+            const verification_token = essential.v_token
+
+            console.log("backend: " + essential.email)
+            console.log("backend: " + essential.v_token)
+
+            const user = await User.findOne({
                 $and: [{
                     "email": email,
                 }, {
                     "verification_token": parseInt(verification_token)
                 }]
             })
-
-            if (user == null){
+            
+            if (user==null){
+                if(email == undefined && verification_token == undefined)   return
                 req.status  = "error"
                 req.message = "Email does not exists or link is expired."
             } else {
-                User.findOneAndUpdate({
+                await User.findOneAndUpdate({
                     $and: [{
                         "email": email,
                     }, {
@@ -27,15 +35,13 @@ export default async (req, res) => {
                     }]        
                 }, {
                     $set: {
-                        "verification_token": "",
-                        "isVerified": true
+                        "verification_token" : "",
+                        "isVerified" : true
                     }        
-                }, (error, data) => {
-                    res.json("Pass")
                 })
                                 
                 req.status = "success"
-                req.message = "Account has been activitated"        
+                req.message = "Account has been activitated"   
             }
 
             //console.log(email, token)
