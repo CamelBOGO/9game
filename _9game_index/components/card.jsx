1fd3dc383dc0e2a0cb758573c7aae0ea529e6ed9
Brian Fong <100516213+BriaaaaanFong@users.noreply.github.com>
2022-04-09 20:42:37 +0800

    Update card.jsx

--- _9game_index/components/card.jsx
+++ _9game_index/components/card.jsx
@@ -3,7 +3,7 @@ import {Box, Typography} from "@mui/material"
 import {makeStyles} from "@material-ui/styles"
 
 
-export default function IndexCard({id, title, content}) {
+export default function IndexCard(props) {
     const cardTextStyle = makeStyles({
         textEllipsis: {
             overflow: "hidden",
@@ -17,8 +17,8 @@ export default function IndexCard({id, title, content}) {
 
     return (
         <Card sx={{width: 300, height: 400, boxShadow: 5}}>
-            <CardActionArea href={`/post/${id}/comnt`}>
-                <Box component="div" sx={{height: 300, p: 2}}>
+            <CardActionArea href={`/post/${id}/comnt`} >
+                <Box component="div" sx={{height: 300, p: 2}} style={{position: "relative", top:"0",width: "auto", marginBottom: "20%"}}>
                     {/*<CardMedia
                         component="img"
                         height="140"
@@ -35,12 +35,13 @@ export default function IndexCard({id, title, content}) {
                                     WebkitLineClamp: '12',
                                     WebkitBoxOrient: 'vertical',
                                 }}>
-                        {content}
+                        {props.content}
                     </Typography>
                 </Box>
-                <CardContent>
-                    <Typography noWrap gutterBottom variant="h5" component="div">
-                        {title}
+                <CardContent style={{position: "absolute", display: "inline-block", bottom:"0", left:"0", width:"auto", height: "10%", padding: "10"}}>
+                    <Typography gutterBottom variant="h5" component="div" style={{width: "auto",display: "inline-flex", justifyContent: "space-between"}}>
+                        <Like id={props.id} style={{bottom: "0", left: "0", padding: "0"}} likes={props.likes} updateLikedPosts={props.updateLikedPosts} likedPosts={props.likedPosts} likeduser={props.likeduser}/>
+                        {props.title}
                     </Typography>
                 </CardContent>
             </CardActionArea>
