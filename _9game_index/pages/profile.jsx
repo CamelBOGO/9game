445e922ad2b4c6bc67a30a7c5d8c3e0ee22ee35e
BOGO <42817357+CamelBOGO@users.noreply.github.com>
2022-04-10 00:58:50 +0800

    Fix Syntax

--- _9game_index/pages/profile.jsx
+++ _9game_index/pages/profile.jsx
@@ -5,46 +5,46 @@ import {useRouter} from "next/router";
 import {Grid, FormControl, InputLabel, Input, Button} from "@mui/material";
 
 
-export default function Profile () {
+export default function Profile() {
     const [images, setImages] = useState([]);
-    const [email,setEmail] = useState("")
+    const [email, setEmail] = useState("")
 
 
-    const onSubmit =async function(e){
+    const onSubmit = async function (e) {
         e.preventDefault()
 
         const config = {
-            headers:{
+            headers: {
                 "Content-Type": "application/json"
             }
         }
 
-        const { data } = await axios.post("/api/profile/profile", { email, images }, config)
+        const {data} = await axios.post("/api/profile/profile", {email, images}, config)
         console.log(data)
     }
     return (
-            <div className="container">
-                <div className="row">
-                    <img />
-                    <form onSubmit={onSubmit}>
-                        <input value = {email} onChange={e => setEmail(e.target.value)}/>
-                        <div className="form-group">
-                            {/* <input type="file" id="profile" accept="image/png, image/jpeg" 
+        <div className="container">
+            <div className="row">
+                <img/>
+                <form onSubmit={onSubmit}>
+                    <input value={email} onChange={e => setEmail(e.target.value)}/>
+                    <div className="form-group">
+                        {/* <input type="file" id="profile" accept="image/png, image/jpeg"
                             value={images}onChange={e => setImages(e.target.value)}>
                             </input> */}
-                          <input  type="file" id="image" 
-                             name="image" value="" required></input>
-                        </div>
-                        <div className="form-group">
-                            <button className="btn btn-primary" type="submit">Upload</button>
-                        </div>
-                    </form>
-                </div>
-                <div>
-                <img src="data:image/<%=image.img.contentType%>;base64,
-                     <%=image.img.data.toString('base64')%>">
-                </div>
+                        <input type="file" id="image"
+                               name="image" value="" required></input>
+                    </div>
+                    <div className="form-group">
+                        <button className="btn btn-primary" type="submit">Upload</button>
+                    </div>
+                </form>
             </div>
-            
+            <div>
+                {/*<img src="data:image/<%=image.img.contentType%>;base64,
+                     <%=image.img.data.toString('base64')%>">*/}
+            </div>
+        </div>
+
     )
 }
