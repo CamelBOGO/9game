d021b39b965a0509956dcb95bac83b48362f805f
BOGO <42817357+CamelBOGO@users.noreply.github.com>
2022-04-09 22:00:03 +0800

    Update card.jsx

--- _9game_index/components/card.jsx
+++ _9game_index/components/card.jsx
@@ -5,6 +5,7 @@ import Like from "../pages/like_post";
 
 
 export default function IndexCard(props) {
+    const id = props.id
     const cardTextStyle = makeStyles({
         textEllipsis: {
             overflow: "hidden",
@@ -18,8 +19,9 @@ export default function IndexCard(props) {
 
     return (
         <Card sx={{width: 300, height: 400, boxShadow: 5}}>
-            <CardActionArea href={`/post/${id}/comnt`} >
-                <Box component="div" sx={{height: 300, p: 2}} style={{position: "relative", top:"0",width: "auto", marginBottom: "20%"}}>
+            <CardActionArea href={`/post/${id}/comnt`}>
+                <Box component="div" sx={{height: 300, p: 2}}
+                     style={{position: "relative", top: "0", width: "auto", marginBottom: "20%"}}>
                     {/*<CardMedia
                         component="img"
                         height="140"
@@ -39,9 +41,20 @@ export default function IndexCard(props) {
                         {props.content}
                     </Typography>
                 </Box>
-                <CardContent style={{position: "absolute", display: "inline-block", bottom:"0", left:"0", width:"auto", height: "10%", padding: "10"}}>
-                    <Typography gutterBottom variant="h5" component="div" style={{width: "auto",display: "inline-flex", justifyContent: "space-between"}}>
-                        <Like id={props.id} style={{bottom: "0", left: "0", padding: "0"}} likes={props.likes} updateLikedPosts={props.updateLikedPosts} likedPosts={props.likedPosts} likeduser={props.likeduser}/>
+                <CardContent style={{
+                    position: "absolute",
+                    display: "inline-block",
+                    bottom: "0",
+                    left: "0",
+                    width: "auto",
+                    height: "10%",
+                    padding: "10"
+                }}>
+                    <Typography gutterBottom variant="h5" component="div"
+                                style={{width: "auto", display: "inline-flex", justifyContent: "space-between"}}>
+                        <Like id={props.id} style={{bottom: "0", left: "0", padding: "0"}} likes={props.likes}
+                              updateLikedPosts={props.updateLikedPosts} likedPosts={props.likedPosts}
+                              likeduser={props.likeduser}/>
                         {props.title}
                     </Typography>
                 </CardContent>
