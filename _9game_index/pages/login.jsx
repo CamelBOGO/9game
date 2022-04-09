e203aeaf5bd4a6d2de801f0b613a9ef749083c83
BOGO <42817357+CamelBOGO@users.noreply.github.com>
2022-04-10 03:37:52 +0800

    Minor Change

--- _9game_index/pages/login.jsx
+++ _9game_index/pages/login.jsx
@@ -1,32 +1,35 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import axios from 'axios'
 import cookies from 'js-cookie'
-
+import {useRouter} from "next/router";
 
 const Register = () => {
-    const [email,setEmail] = useState("")
+    const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
-    const SubmitHandler = async(e) => {
+    const router = useRouter()
+    
+    const SubmitHandler = async (e) => {
         e.preventDefault()
 
         //console.log(email, password)
         const config = {
-            headers:{
+            headers: {
                 "Content-Type": "application/json"
             }
         }
 
-        const { data } = await axios.post("/api/auth/login", { email, password }, config)
+        const {data} = await axios.post("/api/auth/login", {email, password}, config)
         cookies.set('email', data.email)
         cookies.set('token', data.accessToken)
         console.log(data)
+        router.push("/")
     }
     return (
         <div>
             <form onSubmit={SubmitHandler}>
                 <h1>Login</h1>
-                <input value = {email} onChange={e => setEmail(e.target.value)}/>
-                <input value = {password} onChange={e => setPassword(e.target.value)}/>
+                <input value={email} onChange={e => setEmail(e.target.value)}/>
+                <input value={password} onChange={e => setPassword(e.target.value)}/>
                 <button type="submit">Login</button>
             </form>
         </div>
