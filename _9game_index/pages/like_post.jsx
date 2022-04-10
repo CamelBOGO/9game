91028ae6477c3e3caaa04edf789dbc4ad04cf91b
Brian Fong <100516213+BriaaaaanFong@users.noreply.github.com>
2022-04-10 20:11:52 +0800

    Update like_post.jsx

--- _9game_index/pages/like_post.jsx
+++ _9game_index/pages/like_post.jsx
@@ -1,134 +1,55 @@
 import React, {useState} from 'react';
-import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 import Favorite from '@mui/icons-material/Favorite';
 import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
+import { pink } from '@mui/material/colors'
+import { FormControl } from '@mui/material';
 
 export default function Like(props) {
     const id = props.id;
-    const [isLiked, updateLike] = useState(false);
-    const username = "brian@1.com"
+    const username = props.email
 
-    let currentLikePosts = props.likedPosts;
-    let likeduser = props.likeduser;
-    const currentlikeduser = likeduser;
+    let currentlikeduser = props.likeduser
 
-    const [userform, setUser] = useState({
-        email: username,
-        likedPosts: currentLikePosts,
-    })
+    const checkprevious = (currentlikeduser.includes(username))? true: false
+
+    const [checkstate, setcheck] = useState(checkprevious)
+    const label = { inputProps: { 'aria-label': 'Checkbox demo'}};
 
     const [form, setForm] = useState({
         _id: id,
-        likeduser: currentlikeduser,
+        email: username,
+        inside: checkprevious
     })
 
-    const handleLike = async function (e) {
-        e.preventDefault()
+    const tryfetch = async (e) => {
+        e.persist()
+        
+        const check = e.target.checked
+        setcheck(check)
+        try {
+            const res = await fetch('/api/like_api', {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(form),
+            })
 
-        if (!isLiked) {
-            updateLike(true);
-            // if (!currentLikePosts.includes(id)) {
-            //     props.updateLikedPosts(
-            //         currentLikePosts.push(id)
-            //     );
-
-            //     try {
-            //         setForm({
-            //             likeduser: likeduser.push(username),
-            //         })
-            //         setUser({
-            //             likedPosts: currentLikePosts,
-            //         })
-            //         const res = await fetch('/api/like_api', {
-            //             method: 'POST',
-            //             headers: {
-            //                 Accept: 'application/json',
-            //                 'Content-Type': 'application/json',
-            //             },
-            //             body: JSON.stringify(form),
-            //         })
-
-            //         if (!res.ok) {
-            //             throw new Error(res.status)
-            //         }
-
-            //         res = await fetch('/api/user_api', {
-            //                 method: 'POST',
-            //                 headers: {
-            //                     Accept: 'application/json',
-            //                     'Content-Type': 'application/json',
-            //                 },
-            //                 body: JSON.stringify(userform),
-            //         })
-
-            //         if (!res.ok) {
-            //             throw new Error(res.status)
-            //         }
-
-            //     } catch (error) {
-            //         console.error(error)
-            //         console.log("Fail to upload!")
-            //     }
-            // }
-        } else {
-            updateLike(false);
-            // if (currentLikePosts.includes(id)) {
-            //     props.updateLikedPosts(
-            //         currentLikePosts.filter(_id => _id !== id)
-            //     );
-
-            //     try{
-            //         likeduser.filter(likeduser => likeduser !== username);
-            //         setForm({
-            //             likeduser: likeduser,
-            //         })
-            //         setUser({
-            //             likedPosts: currentLikePosts,
-            //         })
-
-            //         const res = await fetch('/api/like_api', {
-            //             method: 'POST',
-            //             headers: {
-            //                 Accept: 'application/json',
-            //                 'Content-Type': 'application/json',
-            //             },
-            //             body: JSON.stringify(form),
-            //         })
-
-            //         if (!res.ok) {
-            //             throw new Error(res.status)
-            //         }
-
-            //         res = await fetch('/api/user_api', {
-            //             method: 'POST',
-            //             headers: {
-            //                 Accept: 'application/json',
-            //                 'Content-Type': 'application/json',
-            //             },
-            //             body: JSON.stringify(userform),
-            //         })
-
-            //         if (!res.ok) {
-            //             throw new Error(res.status)
-            //         }
-            //     } catch (error) {
-            //         console.error(error)
-            //         console.log("Fail to upload!")
-            //     }
-            // }
+            if (!res.ok) {
+                throw new Error(res.status)
+            }
+        } catch (error) {
+            console.error(error)
+            console.log("Fail to upload!")
         }
-    };
+    }
 
     return (
-        // <FormControlLabel
-        //     control={<Checkbox icon={<FavoriteBorder/>}
-        //             checkedIcon={<Favorite/>}
-        //     name="checkedH" onClick={handleLike}
-        //     />}
-        // />
-        <div>
-
-        </div>
+        <FormControl style={{position: "relative", width: "auto", height: "auto", zIndex: "2"}}>
+            <Checkbox {...label} checked={checkstate} icon={<FavoriteBorder/>} checkedIcon={<Favorite sx={{ color: pink[300], '&.Mui-checked': { color: pink[300], }, }} />} onChange={tryfetch}
+                />    
+        </FormControl>
     );
 };
