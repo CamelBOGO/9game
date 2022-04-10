052c7111ea6e865876b325f810f019c0ba0e9142
WaYiu <1155129302@link.cuhk.edu.hk>
2022-04-11 02:22:49 +0800

    Register UI

--- _9game_index/pages/register.jsx
+++ _9game_index/pages/register.jsx
@@ -1,11 +1,17 @@
 import { useState } from 'react'
 import axios from 'axios'
-import { parseCookies } from 'nookies'
+
+import {useRouter} from "next/router";
+import styles from "../styles/authstyle.module.css"
+import {FormControl, Grid, TextField, Typography, Card, Link} from "@mui/material";
 
 
 const Register = () => {
     const [email,setEmail] = useState("")
     const [password, setPassword] = useState("")
+    const [confirmPassword, setConfirmPassword] = useState("")
+    const router = useRouter()
+
     const SubmitHandler = async(e) => {
         e.preventDefault()
 
@@ -16,18 +22,73 @@ const Register = () => {
             }
         }
 
-        const { data } = await axios.post("/api/auth/register", { email, password }, config)
-        console.log(data)
+        const { data } = await axios.post("/api/auth/register", { email, password, confirmPassword }, config)
+        const status = data.status
+        const message = data.message
+
+        if(status == "error"){
+            alert(data.message)
+        } else {
+            alert(data.message)
+            router.push("/login")
+        }
+
     }
     return (
         
-        <div>
-            <form onSubmit={SubmitHandler}>
-                <h1>Register</h1>
-                <input value = {email} onChange={e => setEmail(e.target.value)}/>
-                <input value = {password} onChange={e => setPassword(e.target.value)}/>
-                <button type="submit">Register</button>
-            </form>
+        <div className={styles.body}>
+            <Grid
+                container
+                sx={{pt: 2}}
+                direction="column"
+                alignItems="center"
+                justifyContent="center"
+            >
+                <Card sx={{width: 400, height: 450, boxShadow: 10}}>
+                    <Grid
+                            container
+                            sx={{pt: 2}}
+                            direction="column"
+                            alignItems="center"
+                            justifyContent="center"
+                        >
+                            <Typography variant="h3" sx={{mt: 2}}>Register</Typography>
+
+                            <form className={styles.form} onSubmit={SubmitHandler}>
+                                <Grid
+                                    container
+                                    sx={{pt: 2}}
+                                    direction="column"
+                                    alignItems="center"
+                                    justifyContent="center"
+                                >
+                                    <FormControl>
+                                        <TextField value={email} onChange={e => setEmail(e.target.value)} label="Email"
+                                                autoComplete="off"
+                                                margin="dense" sx={{width: 300}}/>
+                                    </FormControl>
+                                    <br/>
+
+                                    <FormControl>
+                                        <TextField value={password} onChange={e => setPassword(e.target.value)} label="Password"
+                                                autoComplete="off"
+                                                margin="dense" sx={{width: 300}}/>
+                                    </FormControl>
+                                    <br/>
+
+                                    <FormControl>
+                                        <TextField value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)} label="Confirm Password"
+                                                autoComplete="off"
+                                                margin="dense" sx={{width: 300}}/>
+                                    </FormControl>
+                                    <br/>
+
+                                    <button className={styles.button} type="submit">Register</button>
+                                </Grid>
+                            </form>
+                    </Grid>
+                </Card>
+            </Grid>
         </div>
     )
 }
