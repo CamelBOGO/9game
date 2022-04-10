90d32b1458ff0d45823d53f63f35f73265810eb9
Brian Fong <100516213+BriaaaaanFong@users.noreply.github.com>
2022-04-10 20:09:11 +0800

    Update like_api.js

--- _9game_index/pages/api/like_api.js
+++ _9game_index/pages/api/like_api.js
@@ -4,12 +4,19 @@ import Post from "../../db_models/post_model";
 export default async function handler(req, res) {
     const {method, body} = req
     await dbConnect()
+    const inside = body.inside
+    const currentuser = body.email
 
-    const likeduser = body.likeduser;
     switch (method) {
         case "POST":
             try {
-                const post = await Post.findOneAndUpdate({_id: body._id}, {$set:{"likeduser":likeduser}})
+                if (inside) {
+                    var action = {$pull: {"likeduser": currentuser}, $inc: {"likes": -1}}
+                } 
+                if (!inside) {
+                    var action = {$push: {"likeduser": currentuser}, $inc: {"likes": 1}}
+                }
+                const updates = await Post.findOneAndUpdate({_id: body._id}, action)
                 res.status(201).json({success: true, data: post})
             } catch (error) {
                 res.status(400).json({success: false})
