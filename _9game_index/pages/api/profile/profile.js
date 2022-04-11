1c0ddcf2c8180c264df28cf6a4960909f8807b34
MichaelShek <1155128001@link.cuhk.edu.hk>
2022-04-11 23:25:31 +0800

    Update profile.js

--- _9game_index/pages/api/profile/profile.js
+++ _9game_index/pages/api/profile/profile.js
@@ -5,18 +5,18 @@ import User from "../../../db_models/user_model"
 
 export default async (req, res) => {
     await dbConnect()             
-    const {item,user}=req.body
+    const {item,currentUser}=req.body
     try {
         if (req.method == "POST") {
             console.log("get req Post")
-            const newUser = User.findOneAndUpdate({ "email": user},
+            const newUser = User.findOneAndUpdate({ "email": currentUser},
             {$set: { "profileimg": item  }},
             (error, data) =>{
                 console.log("Here!!!!!!!!!!!!!!!!")
+                
                 res.status(200).json({
                     "status": "success",
                     "message": "change successfully.",
-                    "photo":item,
                 })
             });
 
