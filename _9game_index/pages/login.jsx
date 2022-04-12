194940496e7b3c2ba720fe3c1a5992f513bdfc41
WaYiu <1155129302@link.cuhk.edu.hk>
2022-04-12 23:16:10 +0800

    login UI

--- _9game_index/pages/login.jsx
+++ _9game_index/pages/login.jsx
@@ -1,10 +1,10 @@
-import {useState} from 'react'
-import axios from 'axios'
-import cookies from 'js-cookie'
+import {useState} from "react"
+import axios from "axios"
+import cookies from "js-cookie"
 
 import {useRouter} from "next/router";
-import styles from "../styles/authstyle.module.css"
-import {FormControl, Grid, TextField, Typography, Card, Link, Box} from "@mui/material";
+import VpnKeyRoundedIcon from "@mui/icons-material/VpnKeyRounded"
+import {Grid, Paper, Avatar, FormControl, TextField, Link} from "@mui/material";
 
 const Register = () => {
     const [email, setEmail] = useState("")
@@ -29,76 +29,55 @@ const Register = () => {
         if (status == "error") {
             alert(data.message)
         } else {
-            cookies.set('email', data?.email)
-            cookies.set('token', data?.accessToken)
+            cookies.set("email", data?.email)
+            cookies.set("token", data?.accessToken)
             //alert(data.message)
             router.push("/")
         }
     }
 
+    const paperStyle = {padding: 15, minHeight:"65vh", width: 300, margin:"50px auto", borderRadius: "25px"}
+    const avatarStyle = {backgroundColor: "#212121"}
+    const buttonStyle = {margin:"25px", width: 250, height: "50px", border: "1px solid", background: "#212121", borderRadius: "25px", "font-size": "18px", "color": "#e9f4fb"}
     return (
-        <Box className={styles.body} sx={{minHeight: 450}}>
-            <Grid
-                container
-                spacing={0}
-                direction="column"
-                alignItems="center"
-                justifyContent="center"
-                style={{minHeight: '100vh'}}
-            >
+        <Grid align = "center" celled padded style={{fontFamily: "sans-serif", minHeight: "92vh"}}>
+            <style>{"body { background-image: linear-gradient(315deg, #000000 0%, #ffffff 74%);}"}</style>
+            <Paper elevation={20} style={paperStyle}>
+                <div style={{margin:"30px auto"}}>
+                    <Avatar style={avatarStyle}><VpnKeyRoundedIcon/></Avatar>
+                    <h2>Login</h2>
+                </div>
 
-                <Card sx={{width: 350, height: 450, boxShadow: 10}}>
-                    <Grid
-                        container
-                        sx={{pt: 2}}
-                        direction="column"
-                        alignItems="center"
-                        justifyContent="center"
-                    >
+                <form onSubmit={SubmitHandler}>
+                    <FormControl style={{margin:"0px auto", width:250}}>
+                        <TextField value={email} onChange={e => setEmail(e.target.value) }
+                            type="email"
+                            label="Email"
+                            margin="dense"
+                            fullWidth required/>
+                    </FormControl>
+                    <br/>
 
-                        <Typography variant="h3" sx={{mt: 2}}>Login</Typography>
-                        <form className={styles.form} onSubmit={SubmitHandler}>
-                            <Grid
-                                container
-                                sx={{pt: 2}}
-                                direction="column"
-                                alignItems="center"
-                                justifyContent="center"
-                            >
-                                <FormControl>
-                                    <TextField value={email} onChange={e => setEmail(e.target.value)}
-                                               type="email"
-                                               label="email"
-                                               margin="dense" sx={{width: 250}}/>
-                                </FormControl>
-                                <br/>
+                    <FormControl style={{margin:"5px auto", width:250}}>
+                        <TextField value={password} onChange={e => setPassword(e.target.value)}
+                            type="password"
+                            label="Password"
+                            margin="dense"
+                            fullWidth required/>
+                    </FormControl>
+                    <br/>
 
-                                <FormControl>
-                                    <TextField value={password} onChange={e => setPassword(e.target.value)}
-                                               type="password"
-                                               label="Password"
-                                               margin="dense"
-                                               sx={{width: 250, mb: 5}}/>
-                                </FormControl>
+                    <button type="submit" style={buttonStyle}>Login</button>
+                    <br/>
+                </form>
 
-                                <button className={styles.button} type="submit">Login</button>
-                            </Grid>
-                        </form>
-                    </Grid>
-
-                    <Grid
-                        container
-                        sx={{pt: 5}}
-                        direction="row"
-                        alignItems="center"
-                        justifyContent="center"
-                    >
-                        <Link href="/forget" sx={{mx: 3}}>Forget Password?</Link>
-                        <Link href="/register" sx={{mx: 3}}>Sign Up</Link>
-                    </Grid>
-                </Card>
-            </Grid>
-        </Box>
+                <Grid align = "center" style={{margin:"20px auto"}}>
+                    <Link href="/forget" sx={{mx: 3}} style={{textDecoration: "none"}}>Forget Password? </Link>
+                    <Link href="/register" sx={{mx: 3}} style={{textDecoration: "none"}}> Sign Up </Link>
+                </Grid>
+                    
+            </Paper>
+        </Grid>
     )
 }
 
