6b21515f6734ced07b1336ad89a3639a45c93517
BOGO <42817357+CamelBOGO@users.noreply.github.com>
2022-04-12 15:44:28 +0800

    Format

--- _9game_index/pages/api/profile/profile.js
+++ _9game_index/pages/api/profile/profile.js
@@ -2,34 +2,32 @@ import dbConnect from "../../../lib/dbConnect";
 import User from "../../../db_models/user_model"
 
 
-
 export default async (req, res) => {
-    await dbConnect()             
-    const {email,item}=req.body
-    console.log("email:"+email)
-    
+    await dbConnect()
+    const {email, item} = req.body
+    console.log("email:" + email)
+
     try {
         if (req.method == "POST") {
             console.log("get req Post")
             // const abc=User.findOne({"email":email})
             // console.log(abc)
-            const newUser = User.findOneAndUpdate({ "email": email},
-            {$set: { "profileimg": item  }});
-            console.log("newUser:"+newUser)
+            const newUser = User.findOneAndUpdate({"email": email},
+                {$set: {"profileimg": item}});
+            console.log("newUser:" + newUser)
             res.status(201).json({
                 "status": "success",
                 "message": "change successfully.",
-                "photo":item,
+                "photo": item,
             })
         }
 
-    } 
-    catch (error) {
+    } catch (error) {
         console.log(error)
         res.status(422).json({
             "status": "error",
             "message": "Error occur in change password."
         })
     }
-    
+
 }
