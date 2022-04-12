2f484a632d0636eeac54407fef56130dde16b0ae
BOGO <42817357+CamelBOGO@users.noreply.github.com>
2022-04-12 23:36:39 +0800

    Update comment_post.jsx

--- _9game_index/components/commentPost/comment_post.jsx
+++ _9game_index/components/commentPost/comment_post.jsx
@@ -1,9 +1,8 @@
-import React, {useState, useEffect, useRef} from 'react';
+import React, {useState} from 'react';
 import {
     Box,
     Button,
     FormControl,
-    Grid,
     Input,
     InputLabel,
     Typography
@@ -14,14 +13,14 @@ import CommentBlock from './comment_block';
 import Card from '@mui/material'
 
 export default function PostPopUp(props) {
-    const currentuser       = props.currentuser
+    const currentuser = props.currentuser
 
-    const postid            = props.id
-    const postcontent       = props.content
-    const postcomments      = props.comment
-    const postdate          = props.date
-    const postuser          = props.user
-    const check             = props.checked
+    const postid = props.id
+    const postcontent = props.content
+    const postcomments = props.comment
+    const postdate = props.date
+    const postuser = props.user
+    const check = props.checked
     const [form, setForm] = useState({
         post_id: postid,
         user_id: currentuser,
@@ -34,7 +33,7 @@ export default function PostPopUp(props) {
     const handleSubmit = async function (e) {
         e.preventDefault()
         // Here I show you how to post data by calling api.
-        if (currentuser){
+        if (currentuser) {
             try {
                 const res = await fetch('/api/comnt', {
                     method: 'POST',
@@ -68,8 +67,8 @@ export default function PostPopUp(props) {
 
     return (
         <>
-        <Box Container>
-            <Typography gutterBottom variant="body1"
+            <Box Container>
+                <Typography gutterBottom variant="body1"
                             component="div"
                             sx={{
                                 overflow: 'hidden',
@@ -81,57 +80,59 @@ export default function PostPopUp(props) {
                     <Typography>{postuser}</Typography>
                     <Typography>{postdate}</Typography>
                     {postcontent}
-            </Typography>
-        </Box>
-        <Box Container>
-            {/* <Like   id={postid} email={currentuser} checked={check} setCheck={props.setCheck} style={{position: "sticky", left: 0}}/> */}
-            {currentuser? 
-                <Like   id={postid} email={currentuser} checked={check} setCheck={props.setCheck} style={{position: "sticky", left: 0}}/>
-                :""
-            }
-            <Typography>
-                {postdate}
-            </Typography>
-        </Box>
-        
-        <hr/>
+                </Typography>
+            </Box>
+            <Box Container>
+                <Like id={postid} email={currentuser} checked={check} setCheck={props.setCheck}
+                      style={{position: "sticky", left: 0}}/>
+                {currentuser ?
+                    <Like id={postid} email={currentuser} checked={check} setCheck={props.setCheck}
+                          style={{position: "sticky", left: 0}}/>
+                    : ""
+                }
+                <Typography>
+                    {postdate}
+                </Typography>
+            </Box>
 
-        {currentuser?
-            <form onSubmit={handleSubmit}>
-                <FormControl>
-                    <InputLabel htmlFor="text">Content</InputLabel>
-                    <Input
-                        type="text"
-                        name="text"
-                        autoComplete="off"
-                        multiline
-                        onChange={handleChange}
-                        required
-                        sx={{mb: 2}}
-                    />
-                </FormControl>
-                <br/>
-                <Button type="submit">Comment</Button>
-            </form>
-        :""}
-        
-        <Box>
-            {postcomments.data.map((comnt) => (
-                // <Typography
-                //     key={comnt._id}
-                // >   
-                //     {comnt.user_id}: posted on {comnt.date}<br/>
-                //     <span style={{marginLeft: "1rem"}}>
-                //         {comnt.text}
-                //     </span>
-                // </Typography>
-                
-                <Card key={props._id} style={{margin: "0.5rem"}}>
-                    <CommentBlock user_id={comnt.user_id} date={comnt.date} text={comnt.text}/>
-                </Card>
-            ))}
-        </Box>
-        
+            <hr/>
+
+            {currentuser ?
+                <form onSubmit={handleSubmit}>
+                    <FormControl>
+                        <InputLabel htmlFor="text">Content</InputLabel>
+                        <Input
+                            type="text"
+                            name="text"
+                            autoComplete="off"
+                            multiline
+                            onChange={handleChange}
+                            required
+                            sx={{mb: 2}}
+                        />
+                    </FormControl>
+                    <br/>
+                    <Button type="submit">Comment</Button>
+                </form>
+                : ""}
+
+            <Box>
+                {postcomments.data.map((comnt) => (
+                    // <Typography
+                    //     key={comnt._id}
+                    // >
+                    //     {comnt.user_id}: posted on {comnt.date}<br/>
+                    //     <span style={{marginLeft: "1rem"}}>
+                    //         {comnt.text}
+                    //     </span>
+                    // </Typography>
+
+                    <Card key={props._id} style={{margin: "0.5rem"}}>
+                        <CommentBlock user_id={comnt.user_id} date={comnt.date} text={comnt.text}/>
+                    </Card>
+                ))}
+            </Box>
+
         </>
     );
 }
